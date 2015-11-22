f7320a46effd1429b3a6bcb6486a9e1fae685d15
Alexander Bartels <bartels.xander@gmail.com>
2015-11-22 21:57:21 +0100

    implement swproxy api to register as mod

--- src/swproxy-mod-rewrite.js
+++ src/swproxy-mod-rewrite.js
@@ -1,8 +1,36 @@
+
+
+/*eslint-disable no-alert, no-console */
+class ModRewriteRule {
+  constructor(proxy, args) {
+    console.log('Rewrite rule created: ', proxy, args);
+  }
+}
+/*eslint-enable no-alert, no-console */
+
 /**
  * The Proxy for service workers
  */
-class SwProxyModRewrite {
+class ModRewrite {
 
+  /**
+   *
+   * @returns {string} name for the factory method
+   */
+  static factoryMethodName() {
+    return 'rewriteRule';
+  }
+
+  /**
+   *
+   * @param proxy the swproxy instances
+   * @returns {Function} function that returns a factory function to create rules
+   */
+  static factoryMethod(proxy) {
+    return function (...args) {
+      return new ModRewriteRule(proxy, args);
+    };
+  }
 }
 
-export default SwProxyModRewrite;
+export default ModRewrite;
--- test/app.js
+++ test/app.js
@@ -1,10 +1,23 @@
 
-describe('swproxy:mod', function () {
+import { assert } from 'chai';
+
+import ModRewrite from '../src/swproxy-mod-rewrite.js';
+
+
+describe('swproxy:mod-rewrite', function () {
   before(function (done) {
     done();
   });
 
-  it('accept mods as constructor arguments', function () {
+  it('provides a name for the factory method', function () {
+    assert.equal(ModRewrite.factoryMethodName(), 'rewriteRule', 'Name for the mod rule is defined');
+  });
+
+  it('provides a factory method to create a RewriteRule', function () {
+    let factoryMethod = ModRewrite.factoryMethod({mockProxyProperty: 'mock property in a mock class'});
+
+    assert.typeOf(factoryMethod, 'function', 'factory method that expect a proxy as argument is defined');
+    assert.ok(factoryMethod('arg1', 'foo', 'bar'));
 
   });
 });
